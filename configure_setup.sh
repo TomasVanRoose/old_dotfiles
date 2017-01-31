87dd3464f9e99bdcb24501ef47e9769a2d02e377
Tomas Van Roose <tomas.vanroose@ugent.be>
2017-01-31 19:08:57 +0100

    Add Oh-my-zsh to configure script

--- configure_setup.sh
+++ configure_setup.sh
@@ -1,3 +1,7 @@
+# Make sure all the symlinks are in place
+# Todo: script to symlink
+
+
 #Install homebrew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
@@ -18,3 +22,6 @@ brew install python3
 sudo pip install virtualenv
 pip install virtualenvwrapper
 
+# Oh my zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+mv ~/.zshrc.pre-oh-my-zsh ~/.zshrc
