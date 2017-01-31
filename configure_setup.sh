df2b2139d054f5d8f35bcec620a31828548e585f
Tomas Van Roose <tomas.vanroose@ugent.be>
2017-01-31 16:26:57 +0100

    Add configure file with essential brew packages

---
+++ configure_setup.sh
@@ -0,0 +1,11 @@
+#Install homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+brew install git
+brew install neovim
+
+# Install GNU coreutils -> gls for colored ls
+brew install coreutils
+
+# Tree
+brew install tree
